c10f9992d4783a689bee05ea2e28c50a29d75309
Documenter.jl <documenter@juliadocs.github.io>
2021-12-02 13:19:20 +0000

    build based on 0c08ed3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StoppingTutorials","category":"page"},{"location":"#StoppingTutorials","page":"Home","title":"StoppingTutorials","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StoppingTutorials.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StoppingTutorials]","category":"page"}]
+}
